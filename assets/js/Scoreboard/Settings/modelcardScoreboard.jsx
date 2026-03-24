2ab60f3488f6ad87b01eb628e4916448746c8300
Yuxuan Li <lyxsunny0801@gmail.com>
2026-03-24 06:36:18 -0400

    add back modelcard for scoreboard

---
+++ assets/js/Scoreboard/Settings/modelcardScoreboard.jsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import { Typography, Link } from '@mui/material';
+import { MODEL_OPTIONS } from './constants';
+import { MODELCARD_INFO_LOOKUP } from './constants-scoreboard';
+
+const ModelCardNew = ({ region, dataset, model, evalDataset }) => {
+  console.log("ModelCard: model:", model, "region:", region, "dataset:", dataset);
+
+  const getInfo = (dataset, region, model) => {
+    console.log(MODELCARD_INFO_LOOKUP[dataset]?.[region]?.[model]);
+    return MODELCARD_INFO_LOOKUP[dataset]?.[region]?.[model] || { bestLayer: 'unknown', corrScore: 0 };
+  };
+
+  const modelMeta = MODEL_OPTIONS.find(option => option.value === model);
+  const modelName = modelMeta?.label || model;
+  const modelType = modelMeta?.type || 'Unknown Model Type';
+  const cardUrl = modelMeta?.cardUrl || '#';
+
+  const { bestLayer, corrScore } = getInfo(dataset, region, model);
+
+  return (
+    <Box sx={{ minWidth: 250, boxShadow: 3, borderRadius: 2, my: 2}}>
+      <Card variant="outlined" sx={{ borderRadius: 2 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Model Card
+          </Typography>
+          <Typography variant="h5" component="div">
+            <Link href={cardUrl} target="_blank" rel="noopener noreferrer" underline="hover">
+              {modelName}
+            </Link>
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {modelType}
+          </Typography>
+          {evalDataset === "" && (
+              <Typography variant="body2">
+            The optimal model layer for the selected ROI({region.toUpperCase()}):{' '}
+            <strong>{bestLayer}</strong>, 
+            with highest correlation raw score: <strong>{corrScore.toFixed(2)}</strong>, 
+            evaluated on the selected fMRI dataset: <strong>{dataset}</strong>
+          </Typography>
+          )}
+          
+          
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+export default ModelCardNew;
