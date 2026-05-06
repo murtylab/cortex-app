c3ed43387e265b130e3ec3e050f9a1303a26c705
Mayukh Deb <53133634+Mayukhdeb@users.noreply.github.com>
2026-05-06 11:58:19 -0400

    switch to new endpoint

--- assets/js/services/config.js
+++ assets/js/services/config.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = 'https://79c8cee62f0e8e542b.gradio.live';
+export const SERVER_URL = 'https://expanding-convenient-securities-shoppers.trycloudflare.com';
 export const SERVER_URL_Whole_Brain = 'https://f75917c6f7f762acc7.gradio.live';
 
