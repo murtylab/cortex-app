9327dad3f5b9cfbc71309c35610077633d1196ff
kushal-dudipala <134854988+kushal-dudipala@users.noreply.github.com>
2026-05-14 13:46:34 -0400

    slash remove

--- assets/js/services/config.js
+++ assets/js/services/config.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = 'https://belongs-angeles-citizenship-dealing.trycloudflare.com/';
+export const SERVER_URL = 'https://belongs-angeles-citizenship-dealing.trycloudflare.com';
 export const SERVER_URL_Whole_Brain = 'https://a45c7de3b94b3364e0.gradio.live';
 
