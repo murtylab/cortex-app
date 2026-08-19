808957c575c6eda0944156e55578a62971c06450
Ruolin Wang <violynne10@gmail.com>
2026-08-19 19:51:45 -0400

    update gradio link

--- assets/js/services/config.js
+++ assets/js/services/config.js
@@ -1,4 +1,4 @@
 // do not add any trailing backslashes in either URLs. Will break upload
 export const SERVER_URL = 'https://handled-beam-buried-travelers.trycloudflare.com';
-export const SERVER_URL_Whole_Brain = 'https://2100193c31bdf5ec83.gradio.live';
+export const SERVER_URL_Whole_Brain = 'https://056a655593b6fa735f.gradio.live';
 
