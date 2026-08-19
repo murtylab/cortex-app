c72499b238f4dbea26b9df643c69f3cba648fa08
Ruolin Wang <violynne10@gmail.com>
2026-08-19 18:15:47 -0400

    update whole-brain backend

--- assets/js/services/config.js
+++ assets/js/services/config.js
@@ -1,4 +1,4 @@
 // do not add any trailing backslashes in either URLs. Will break upload
 export const SERVER_URL = 'https://handled-beam-buried-travelers.trycloudflare.com';
-export const SERVER_URL_Whole_Brain = 'https://a45c7de3b94b3364e0.gradio.live';
+export const SERVER_URL_Whole_Brain = 'https://2100193c31bdf5ec83.gradio.live';
 
