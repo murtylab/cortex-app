6a308a0e91e03c3a10deb6bda4a052ea2edf96e7
kushal-dudipala <134854988+kushal-dudipala@users.noreply.github.com>
2026-08-21 18:56:41 -0400

    Update SERVER_URL to new endpoint

--- assets/js/services/config.js
+++ assets/js/services/config.js
@@ -1,4 +1,4 @@
 // do not add any trailing backslashes in either URLs. Will break upload
-export const SERVER_URL = 'https://handled-beam-buried-travelers.trycloudflare.com';
+export const SERVER_URL = 'https://committee-bags-realistic-lite.trycloudflare.com';
 export const SERVER_URL_Whole_Brain = 'https://9c68b099094bc39651.gradio.live';
 
